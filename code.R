04420bfd78696c60738551d171bcc10cd6652f4e
Herlinde Wynendaele <herlinde.wynendaele@ugent.be>
2025-05-23 13:17:10 +0200

    test

--- code.R
+++ code.R
@@ -5,3 +5,13 @@ library(ggplot2)
 
 
 # Test 3
+
+# ik wil iets testen. 
+
+R.version.string
+C:\Program Files\R\
+list.files("C:/Program Files/R")
+
+
+# Toon waar je actieve R-installatie nu vandaan komt
+R.home()
